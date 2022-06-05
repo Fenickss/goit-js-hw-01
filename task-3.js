eba366f8489015296a730768d2f424e123989705
Fenickss <denis.suxov.1991@mail.ru>
2022-06-05 14:28:12 +0300

    js

--- task-3.js
+++ task-3.js
@@ -15,6 +15,15 @@ const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
 ```*/
 
-const ADMIN_PASSWORD = 'jqueryismyjam';
-let message;
+const ADMIN_PASSWORD = "jqueryismyjam";
+
+let message = prompt(`введите пароль`);
+
+if (message === ADMIN_PASSWORD ){
+console.log(`Добро пожаловать`);
+}else if(message === null){
+  console.log(`Отменено пользователем!`);
+}else{
+  console.log(`Доступ запрещен, неверный пароль!`);
+}
 
